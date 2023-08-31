c03fd1acd9e58c3f0e01dafd00a351d3c53b7658
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-08-31 08:55:29 +0000

    solved Navbar

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <div className="h-screen w-full relative p-10 pt-16">
+        <div className="h-screen w-full relative p-10 pt-[21vh]">
             <h1 className="text-5xl">
                 This is the Hero Section
             </h1>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,45 @@
 "use client";
 
-import ThemeMenuButton from "./ThemeButton";
+import { useEffect, useState } from "react";
+
+import { Card, CardContent, CardHeader } from "./ui/card";
 import { DropdownHamMenu } from "./DropDownMenu";
+import ThemeMenuButton from "./ThemeButton";
 
 export default function Navbar() {
-    return (
-        <div className="h-16 w-full top-0 z-50 fixed flex items-center justify-between px-10">
-            <div>
-                <h1 className="font-bold text-2xl tracking-widest cursor-pointer">
-                    IETE
-                </h1>
-            </div>
 
-            <div className="flex gap-2 items-center justify-center">
-                <ThemeMenuButton />
-                <DropdownHamMenu />
-            </div>
-        </div>
+    const [top, setTop] = useState(5);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentTop = window.scrollY;
+        if (currentTop > 0) {
+          setTop(-16);
+        } else {
+          setTop(5);
+        }
+      };
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
+    return (
+      <div className="flex justify-center items-center transition-all">
+        <Card className={`h-16 top-${top} w-[95%] z-50 fixed flex items-center justify-between transition-all`}>
+          <CardHeader>
+            <h1 className="font-bold text-2xl tracking-widest cursor-pointer">
+              IETE
+            </h1>
+          </CardHeader>
+
+          <CardContent className="gap-2 flex items-center justify-center">
+            <ThemeMenuButton />
+            <DropdownHamMenu />
+          </CardContent>
+        </Card>
+      </div>
     );
 }
