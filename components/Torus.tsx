a11a4662613e4d38587c5bb336298d08a466e58e
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-09-04 03:20:41 +0000

    added text in torus

--- components/Torus.tsx
+++ components/Torus.tsx
@@ -1,34 +1,44 @@
-'use client';
+"use client";
 
-import { useRef } from 'react';
-import { Mesh } from 'three';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from "react";
+import { Mesh } from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Text } from "@react-three/drei";
 
 function TorusGeometry() {
-    const MeshRef = useRef<Mesh>(null);
+  const meshRef = useRef<Mesh>(null);
 
-    useFrame(() => {
-        if (!MeshRef.current) {
-            return;    
-        }
+  useFrame(() => {
+    if (!meshRef.current) {
+      return;
+    }
 
-        MeshRef.current.rotation.z -= 0.01;
-    });
+    meshRef.current.rotation.z -= 0.01;
+  });
 
-    return (
-      <>
-        <mesh ref={MeshRef}>
-          <torusGeometry args={[2, 0.25, 30, 64]} />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-      </>
-    );
+  return (
+    <>
+      <mesh ref={meshRef}>
+        <torusGeometry args={[2, 0.25, 30, 64]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+    </>
+  );
 }
 
 export default function Torus() {
-    return (
-        <Canvas>
-            <TorusGeometry />
-        </Canvas>
-    )
+  return (
+    <Canvas>
+      <OrbitControls 
+        enableRotate={false}
+        enablePan={true}
+        enableZoom={false}
+      />
+      <ambientLight />
+      <TorusGeometry />
+      <Text scale={0.3} position={[1.5, 1, 1]} >
+        Institute of Electronics and Telecommunication Engineers, Chennai.
+      </Text>
+    </Canvas>
+  );
 }
