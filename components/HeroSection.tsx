fdff94955c70076124b6924cbe56727b1ac27446
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-09-06 10:55:35 +0000

    solved HeroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,12 +2,12 @@ import Torus from "./Torus";
 
 export default function Hero() {
     return (
-        <div className="h-screen w-full p-10 flex-col md:flex items-center justify-center gap-[2rem]">
-            <div className="h-[90%] md:h-[60%] w-[90%] md:w-[60%]">
+        <div className="h-[125vh] mb-[4rem] w-full p-10 flex-col items-center justify-center gap-[2rem]">
+            <div className="h-[80%] w-[80%] m-auto flex items-center justify-center">
                 <Torus />
             </div>
 
-            <div>
+            <div className="h-[25%] w-full flex items-center justify-center">
                 <h1 className="text-2xl md:text-4xl font-bold">
                     Learning Today, <br></br>
                     Leading Tomorrow.
