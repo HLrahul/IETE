b1aeabc32271d23423b5ac2b93f4215da7ac4a47
HLrahul <rahuls10org@gmail.com>
2023-09-06 02:17:10 -0700

    few updates

---
+++ app/commity-members/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="min-h-screen w-full">
+            <Navbar />
+        </div>
+    );
+}
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,9 +2,16 @@ import Torus from "./Torus";
 
 export default function Hero() {
     return (
-        <div className="h-screen w-full p-10 flex items-center justify-center">
-            <div className="h-[90%] w-[90%]">
+        <div className="h-screen w-full p-10 flex-col md:flex items-center justify-center gap-[2rem]">
+            <div className="h-[90%] md:h-[60%] w-[90%] md:w-[60%]">
                 <Torus />
+            </div>
+
+            <div>
+                <h1 className="text-2xl md:text-4xl font-bold">
+                    Learning Today, <br></br>
+                    Leading Tomorrow.
+                </h1>
             </div>
         </div>
     );
