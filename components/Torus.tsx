29c7057889b1a1831cefd66df671a64668f1a8db
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-09-11 03:57:14 +0000

    merged

--- components/Torus.tsx
+++ components/Torus.tsx
@@ -1,35 +1,10 @@
 "use client";
 
 import { useEffect, useRef, useState } from 'react';
-import { BufferAttribute, BufferGeometry, Mesh } from 'three';
+import { Mesh } from 'three';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useTheme } from 'next-themes';
-
-function HollowTriangleGeometry() {
-  const geometry = new BufferGeometry();
-  const vertices = new Float32Array([
-    -1, 1, 0, 1, 1, 0, 0, -1, 0, -0.8, 0.8, 0, 0.8, 0.8, 0, 0, -0.8, 0,
-  ]);
-  const indices = new Uint16Array([0, 1, 2, 3, 4, 5]);
-  geometry.setAttribute("position", new BufferAttribute(vertices, 5));
-  geometry.setIndex(new BufferAttribute(indices, 1));
-
-  useFrame(() => {
-    if (!geometry) {
-      return;
-    }
-
-    // geometry.rotateX(0.01);
-    // geometry.rotateY(0.01);
-  });
-
-  return (
-    <mesh>
-      <bufferGeometry attach="geometry" {...geometry} />
-    </mesh>
-  );
-}
 
 function TorusMesh () {
     const MeshRef = useRef<Mesh>(null);
@@ -46,20 +21,15 @@ function TorusMesh () {
     const {theme} = useTheme();
 
     useEffect(() => {
-        if(theme === "dark") setColor("#fff");
-        else setColor("black");
-    }, [theme]);
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const listener = (event: MediaQueryListEvent | MediaQueryList) => {
+          setColor(event.matches ? "#ffffff" : "#000000");
+        };
+        listener(mediaQuery);
+        mediaQuery.addEventListener("change", listener);
 
-useEffect(() => {
-  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-  const listener = (event: MediaQueryListEvent | MediaQueryList) => {
-    setColor(event.matches ? "#fff" : "black");
-  };
-  listener(mediaQuery);
-  mediaQuery.addEventListener("change", listener);
-  return () => mediaQuery.removeEventListener("change", listener);
-}, []);
-    
+        return () => mediaQuery.removeEventListener("change", listener);
+    }, [theme]);
 
     return (
         <mesh ref={MeshRef}>
@@ -72,8 +42,6 @@ useEffect(() => {
 export default function Torus () {
 
     return (
-
-
         <Canvas>
             <OrbitControls
                 enableRotate={true}
@@ -81,9 +49,7 @@ export default function Torus () {
                 enableZoom={false}
             />
             <ambientLight />
-            {/*<HollowTriangleGeometry />*/}
             <TorusMesh />
         </Canvas>
-
     )
 }
